b5dfccd82e3582a96ca85eb76b2e8c5788e4cb7e
Victor Caldas <victor.solid3@gmail.com>
2020-02-27 12:17:02 -0300

    effect for  the florest section

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
-const menu = document.querySelectorAll('[data-animais="menu"] li');
-const itemMenu = document.querySelectorAll('[data-animais="item"]');
-console.log(itemMenu);
-itemMenu[0].classList.add('ativo');
-
-function activeTab(index) {
-  itemMenu.forEach((item) => {
-    item.classList.remove('ativo');
-  })
-  itemMenu[index].classList.add('ativo');
-}
-menu.forEach((item, index) => {
-  item.addEventListener('click', () => {
-    activeTab(index);
+export default function initTabNav() {
+  const menu = document.querySelectorAll('[data-animais="menu"] li');
+  const itemMenu = document.querySelectorAll('[data-animais="item"]');
+  const menuFloresta = document.querySelectorAll('[data-floresta="menu"] li')
+  const itemFloresta = document.querySelectorAll('[data-floresta="item"]')
+  itemFloresta[1].classList.add('ativo')
+  itemMenu[0].classList.add('ativo');
+  function activeTab(index) {
+    itemMenu.forEach((item) => {
+      item.classList.remove('ativo');
+    })
+    itemMenu[index].classList.add('ativo');
+  }
+  menu.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      activeTab(index);
+    });
   });
-});
+  function activeTab2(index) {
+    itemFloresta.forEach((item) => {
+      item.classList.remove('ativo')
+    })
+    itemFloresta[index].classList.add('ativo');
+  }
+  menuFloresta.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      activeTab2(index);
+    })
+  })
+}
+
