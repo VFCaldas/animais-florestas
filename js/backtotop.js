f6dc74eff80ac1b6d4a242ab54f82d9e77cdf186
Victor Caldas <victor.solid3@gmail.com>
2020-02-28 13:40:07 -0300

    animation on scroll and fishined the form

---
+++ js/backtotop.js
@@ -0,0 +1,29 @@
+
+export default function initScrollTop() {
+
+  const button = document.querySelector('[data-button="scrolltop"]');
+
+  function trackScroll() {
+    const scrolled = window.pageYOffset;
+    const coords = document.documentElement.clientHeight;
+    if (scrolled > coords) {
+      button.classList.add('ativo');
+    }
+    if (scrolled < coords) {
+      button.classList.remove('ativo');
+    }
+
+  }
+
+  function scrollTop(event) {
+    event.preventDefault();
+    const buttonTop = window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    })
+    return buttonTop;
+  }
+  window.addEventListener('scroll', trackScroll);
+  button.addEventListener('click', scrollTop)
+}
